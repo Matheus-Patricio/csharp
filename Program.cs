8028678be94d27a58a947827d553773c899d22e0
Matheus Patricio <contato.matheuspatricio@gmail.com>
2024-09-03 22:12:39 -0300

    first code in csharp

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FirstProject {
+    class NuxtBank{
+            public static void Main(string[] args){
+                NuxtUser  JohnUser = new NuxtUser();
+                JohnUser.Balance = 0;
+                JohnUser.UserName = "John da Silva";
+                JohnUser.DepositInAccount(1000);
+                JohnUser.ShowBalance();
+            }
+
+    }
+
+    public class NuxtUser {
+        public string UserName;
+        public decimal Balance;
+
+        public void DepositInAccount(decimal amount) {
+            Balance +=  amount;
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine($"{UserName} possui saldo de {Balance}");
+        }
+    }
+}
